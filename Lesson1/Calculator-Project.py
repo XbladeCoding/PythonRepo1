b215c459427602a4262c4aa9cf966c8d932b85e9
XbladeCoding <rohan.balasubramanian.2010@gmail.com>
2024-03-26 18:52:45 -0400

    Calculator thingy

---
+++ Lesson1/Calculator-Project.py
@@ -0,0 +1,32 @@
+from math import sqrt
+
+firstPrompt = input('Welcome to Python Calculator! What would you like to do? ')
+
+#First we create basic arthmetic operations
+try:
+    if firstPrompt == 'add' or firstPrompt == 'Add':
+        firstNumAdd = float(input('First number? '))
+        secNumAdd = float(input('Second number? '))
+        print(firstNumAdd + secNumAdd)
+    elif firstPrompt == 'subtract' or firstPrompt == 'Subtract':
+        firstNumSub = float(input('First number? '))
+        secNumSub = float(input('Second Number? '))
+        print(firstNumSub - secNumSub)
+    elif firstPrompt == 'multiply' or firstPrompt == 'Multiply':
+        firstNumMult = float(input('First number? '))
+        secNumMult = float(input('Second numbar? '))
+        print(firstNumMult * secNumMult)
+    elif firstPrompt == 'divide' or firstPrompt == 'Divide':
+        firstNumDiv = float(input('First number? '))
+        secNumDiv = float(input('Second number? '))
+        print(firstNumDiv / secNumDiv)
+    elif firstPrompt == 'exponent' or firstPrompt == 'Exponent':
+        firstNumExp = float(input('Base? '))
+        secNumExp = float(input('Exponent? '))
+        print(firstNumExp ** secNumExp)
+    elif firstPrompt == 'sqrt' or firstPrompt == 'square root':
+        BaseRoot = float(input('Base? '))
+        print(sqrt(BaseRoot))
+
+except ZeroDivisionError:
+    print('DIVIDE BY 0 ERROR')
